912641d3f958e993c1ba9584824004b3dbbfbaed
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2025-05-19 15:53:34 -0300

    fix header on phone

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { PopoverHeader } from "./popover-header";
 export const Header = () => {
     return (
         <header className="w-full flex justify-center p-4 font-bold bg-zinc-900">
-            <div className="flex flex-row justify-between w-full max-w-[1400px] px-4 items-center">
+            <div className="flex flex-col sm:flex-row gap-2 justify-between w-full max-w-[1400px] px-4 items-center">
                 <h3>DIÓXIDO DE SILÍCIO</h3>
                 <div className="flex gap-2">
                     <PopoverHeader title={"Integrantes"}>
